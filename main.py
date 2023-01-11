ec0e27b94e28b970e72fd44a464cebc0ae1e67c8
Jack Sharples <j.sharples1@gmail.com>
2023-01-11 16:57:35 +0000

    New model integrated

--- main.py
+++ main.py
@@ -10,14 +10,10 @@ import streamlit as st
 import plotly.express as px
 import numpy as np
 import pandas as pd
-from streamlit_option_menu import option_menu
-from streamlit_option_menu import option_menu
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-from pyplutchik import plutchik
-import matplotlib.pyplot as plt
-from skimage import io
 from new_image import load_image,resize, convert_to_np
+import os
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
 
@@ -27,8 +23,8 @@ st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_ht
 
 
 with st.sidebar:
-    tabs = on_hover_tabs(tabName=['Main', 'Predict', 'Analysis','Future','Team'],
-                         iconName=['home', 'sync', 'hub','rocket_launch','groups'], default_choice=0)
+    tabs = on_hover_tabs(tabName=['Main', 'Predict','Team'],
+                         iconName=['home', 'sync','groups'], default_choice=0)
 
 if tabs =='Main':
     def load_lottieurl(url:str):
@@ -151,7 +147,7 @@ elif tabs == 'Predict':
             #     st.write("slider", slider_val, "checkbox", checkbox_val)
 
             st.markdown("## No idea? Get a title")
-            openai.api_key = st.secrets['openai_key']
+            # openai.api_key = st.secrets['openai_key']
 
             title3=st.text_input("Use GPT-3 to generate a title:")
 
@@ -241,9 +237,10 @@ elif tabs == 'Predict':
 
             pred_dict = {'image' : images,'text':titles }
 
-            response = requests.post('https://youtube1-nct5cxzhzq-ew.a.run.app/test_predict', json=json.dumps(pred_dict))
+            response = requests.post('https://youtube2-nct5cxzhzq-ew.a.run.app/test_predict', json=json.dumps(pred_dict))
             #response = requests.post('http://127.0.0.1:8000/test_predict', json=json.dumps(pred_dict))
-
+            st.write(response)
+            st.write(response.json)
             result = response.json()
 
             prediction = f"[{result['prediction'][1:-1].replace(']', '],')[:-1]}]"
@@ -282,230 +279,6 @@ elif tabs == 'Predict':
                     st.empty()
                 st.empty()
                 st.markdown("---")
-
-
-
-
-
-elif tabs == 'Analysis':
-    emotions = {'joy': 0.6,
-            'trust': 0.4,
-            'fear': 0.2,
-            'surprise': 0.7,
-            'sadness': 0.4,
-            'disgust': 0.1,
-            'anger': 0.4,
-            'anticipation': 0.35}
-
-    emotions2 = {'joy': 0.5,
-                'trust': 0.4,
-                'fear': 0.3,
-                'surprise': 0.7,
-                'sadness': 0.4,
-                'disgust': 0.1,
-                'anger': 0.4,
-                'anticipation': 0.3}
-
-
-    def make_pie(df):
-
-        data = [(df['anger'] != 0).sum(), (df['anticip'] != 0).sum(),
-            (df['trust'] != 0).sum(), (df['surprise'] != 0).sum(),
-            (df['sadness'] != 0).sum(), (df['disgust'] != 0).sum(),
-            (df['joy'] != 0).sum()]
-
-        keys = ['Anger', 'Anticipation', 'Trust', 'Surprise', 'Sadness', 'Disgust', 'Joy']
-
-        fig = px.pie(df, values=data, names=keys, title='Sentiment Pie Chart')
-
-        return fig
-
-    def make_vader_pie(df):
-
-        data = [(df['neg'] != 0).sum(), (df['neu'] != 0).sum(),
-                (df['pos'] != 0).sum()]
-
-        keys = ['neg', 'neu', 'pos']
-
-        fig = px.pie(df, values=data, names=keys, title='Sentiment Pie Chart', hole=.3)
-
-        return fig
-
-
-    vader_top10 = pd.read_csv('data/vader_top10.csv')
-
-    vader_bottom10 = pd.read_csv('data/vader_bottom10.csv')
-
-
-    # st.markdown(get_css(), unsafe_allow_html=True)
-
-    # 2. horizontal menu
-    selected2 = option_menu(None, ["Click Triggers", "Word Cloud", "Sentiment Analysis"],
-        icons=['hand-index', "emoji-smile", "cloud"],
-        menu_icon="cast", default_index=0, orientation="horizontal",
-        styles={
-            "container": {"padding": "0!important", "background-color": "#fafafa"},
-            "icon": {"color": "orange", "font-size": "20px"},
-            "nav-link": {"font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-            "nav-link-selected": {"background-color": "red"},
-        }
-    )
-
-    if selected2 == "Click Triggers":
-        with st.container():
-            col1, col2= st.columns([1,1])
-            with col1:
-                A = pd.DataFrame([['With','Capitals',0,0],
-                            ['With','Capitals',3794,1],
-                            ['Without','Capitals',0,0],
-                            ['Without','Capitals',633,1],
-                            ['With','Emojis',0,0],
-                            ['With','Emojis',1402,1],
-                            ['Without','Emojis',0,0],
-                            ['Without','Emojis',633,1],
-                            ['With','Numbers',0,0],
-                            ['With','Numbers',1955,1],
-                            ['Without','Numbers',0,0],
-                            ['Without','Numbers',425,1]],columns = ['with','trick','views','play'])
-
-
-                base = (((1.77*29)+(0.668*3))/32)
-                C = pd.DataFrame([['1',0,0],
-                                ['1',4.4/base,1],
-                                ['2',0,0],
-                                ['2',4.02/base,1],
-                                ['3+',0,0],
-                                ['3+',base/base,1]],columns = ['Number','Proportional views','play'])
-
-                trick_bar = st.selectbox("",options=['Capitals', 'Emojis', 'Numbers', 'Faces'])
-
-                if trick_bar == 'Faces':
-
-                    fig1 = px.bar(C, x='Number', y='Proportional views', color='Number', range_y=[0,4], animation_frame="play",animation_group="Number")
-                    fig1.update_xaxes(visible=False, showticklabels=False)
-                    fig1.update_layout(title=trick_bar, legend_title="")
-
-                else:
-                    B = A[A['trick'].isin([trick_bar])]
-                    fig1 = px.bar(B, x='with', y='views', color='with', range_y=[0,4000], animation_frame="play",animation_group="with")
-                    fig1.update_xaxes(visible=False, showticklabels=False)
-                    fig1.update_layout(title=trick_bar, legend_title="")
-                # st.write(px.bar(B, x='with', y='views', color='with', range_y=[0,4000]))
-
-                # trick_bar = st.selectbox(
-                #         ' ', options=['Capitals', 'Emojis', 'Numbers'])
-
-                st.write(fig1)
-
-            with col2:
-                st.write("")
-                st.write("")
-                if trick_bar == 'Capitals':
-                    with st.expander("See explanation"):
-                        st.write("""
-                        We found that simply having at least one word in all caps resulted in an average threefold increase in views.
-                    """)
-                        st.image("images/mouth.png", width=500)
-                elif trick_bar == 'Emojis':
-                    with st.expander("See explanation"):
-                        st.write("""
-                            We found that emojis were correlated with higher view count on average, when controlling for outliers.
-                        """)
-                        st.image("images/emoji.jpeg", width=500)
-                elif trick_bar == 'Numbers':
-                    with st.expander("See explanation"):
-                        st.write("""
-                                We found having at least one number in a title significantly improves average view score. We understood this correltion to be in part explainable by the prevelance of 'listicles' with high view counts.
-                        """)
-                        st.image("images/blog.webp", output_format='PNG', width=500)
-                elif trick_bar == 'Faces':
-                    with st.expander("See explanation"):
-                        st.write("""
-                                We ran the CV2 Face Detection Model on the dataset, and found having more than two faces was detrimental to view count.
-                        """)
-                        st.image("images/marylin.jpeg", output_format='PNG', width=500)
-
-
-    elif selected2 == "Word Cloud":
-        col1, col2 = st.columns([2,2], gap='medium')
-        with col1:
-            st.title('Best performing 10% of videos')
-            st.image('images/topstop.png')
-            with st.expander("See explanation"):
-                        st.write("""
-                        After removing common stopwords, and hashtags for categories like #shorts, we found that the most common word in the most successful 10% of videos was 'vs'. This indicates that including 'vs' in your title may also act as a click trigger.
-                    """)
-
-
-        with col2:
-            st.title('Worst performing 10% of videos')
-            st.image('images/bottomstop.png')
-            with st.expander("See explanation"):
-                        st.write("""
-                        After removing stopwords and hashtags, we found the most common words in the least well performing 10% of videos to be as above. Youtubers don't like love.. :(
-                    """)
-
-    elif selected2 == "Sentiment Analysis":
-        col1, col2 = st.columns([2,2], gap='small')
-        with col1:
-            st.subheader("Sentiment distribution of top 10% by viewcount")
-            fig = make_vader_pie(vader_top10)
-            fig.update_layout(title='')
-            fig.update_traces(textposition='inside', textinfo='percent+label')
-            fig.update_layout(showlegend=False)
-            st.plotly_chart(fig)
-
-
-            # fig1=make_pie(top10)
-            # fig1.update_layout(title='Emotion distribution of top 10% of videos by viewcount')
-            # st.plotly_chart(fig1)
-            st.write("")
-            st.write("")
-            st.subheader("Best Performing 10% emotion detection")
-            fig, ax = plt.subplots(1, figsize=(7,7))
-            plutchik(emotions, ax, fontweight = 'light',
-                fontsize = 10)
-            st.pyplot(fig)
-
-        with col2:
-            st.subheader("Sentiment distribution of bottom 10% by viewcount")
-            fig3 = make_vader_pie(vader_bottom10)
-            fig3.update_layout(title='')
-            fig3.update_traces(textposition='inside', textinfo='percent+label')
-            st.plotly_chart(fig3)
-
-
-            # fig2=make_pie(bottom10)
-            # fig2.update_layout(title='Emotion distribution of bottom 10% of videos by viewcount')
-            # st.plotly_chart(fig2)
-            st.write("")
-            st.write("")
-            st.subheader("Worst performing 10% emotion detection")
-            fig, ax = plt.subplots(1, figsize=(7,7))
-            plutchik(emotions2, ax, fontweight = 'light',
-                fontsize = 10)
-            st.pyplot(fig)
-
-elif tabs == 'Future':
-    st.markdown("# Not feeling creative? Here's a thumbnail for you !")
-
-    def image_generate_dalle(text):
-        openai.api_key =st.secrets['dalle']
-        response = openai.Image.create(
-        prompt= f'youtube thumbnail about {text}',
-        n=1,
-        size="1024x1024"
-        )
-        image_url = response['data'][0]['url']
-
-        return image_url
-
-    key_words=st.text_input("Use DALL-E to generate a thumbnail:")
-
-    image_url=image_generate_dalle(key_words)
-
-    a=io.imread(image_url)
-    st.image(a)
 
 elif tabs == 'Team':
     st.title('ABOUT US')
